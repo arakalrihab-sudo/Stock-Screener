936ee995d2e2c717a22093c96cb72b35456d3f1e
RihabArakal <arakal.rihab@gmail.com>
2026-04-12 09:17:11 -0400

    Add gitignore and remove pycache

--- check.py
+++
@@ -1,9 +0,0 @@
-from data.storage import get_all_sentiment
-
-results = get_all_sentiment()
-
-if not results:
-    print("No sentiment data found")
-else:
-    for row in results:
-        print(row)
